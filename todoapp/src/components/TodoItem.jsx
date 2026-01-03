91961e1619789477b051972dbfb1a5874c1ea28c
= <=>
2026-01-03 19:17:38 +0530

    adding completed todo and sort todos based on completion

--- todoapp/src/components/TodoItem.jsx
+++ todoapp/src/components/TodoItem.jsx
@@ -3,10 +3,20 @@ export default function TodoItem({ item, todos, setTodos }) {
   function handleDelete(item) {
     setTodos(todos.filter((todo) => todo !== item));
   }
+  function handleClick(name) {
+    const newArray = todos.map((todo) =>
+      todo.name === name ? { ...todo, done: !todo.done } : todo
+    );
+    setTodos(newArray);
+  }
+  const completed = item.done ? styles.completed : "";
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item}
+        <span className={completed} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
+
         <span>
           <button onClick={() => handleDelete(item)} className={styles.button}>
             x
